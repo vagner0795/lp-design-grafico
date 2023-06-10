515a5ab6c0dd64f04066854ae1c4823f0fcef030
vagner0795 <vagner-se@hotmail.com>
2023-06-10 12:46:48 -0300

    feat: landing Page Designer Grafico versao 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
+// const q = document.querySelectorAll('.q');
+// const a = document.querySelectorAll('.a');
+// const arr = document.querySelectorAll('.arrow');
+
+// // SELECIONAR TODOS OS ELEMENTOS 'Q'
+// for(let i = 0; i < q.length; i++) {
+//     q[i].addEventListener('click', () => {
+//         a[i].classList.toggle('a-oppened');
+//         arr[i].classList.toggle('arrow-rotated');
+
+//     });
+
+// }
+
 const q = document.querySelectorAll('.q');
 const a = document.querySelectorAll('.a');
 const arr = document.querySelectorAll('.arrow');
 
 // SELECIONAR TODOS OS ELEMENTOS 'Q'
 for(let i = 0; i < q.length; i++) {
-    q[i].addEventListener('click', () => {
-        a[i].classList.toggle('a-oppened');
-        arr[i].classList.toggle('arrow-rotated');
-    });
+  q[i].addEventListener('click', () => {
+    // FECHAR TODAS AS RESPOSTAS ABERTAS
+    for(let j = 0; j < a.length; j++) {
+      if(j !== i && a[j].classList.contains('a-oppened')) {
+        a[j].classList.remove('a-oppened');
+        arr[j].classList.remove('arrow-rotated');
+      }
+    }
+    
+    // ABRIR/FECHAR A RESPOSTA CLICADA
+    a[i].classList.toggle('a-oppened');
+    arr[i].classList.toggle('arrow-rotated');
+  });
 }
