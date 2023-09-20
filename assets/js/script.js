aaddd2cd649124d895f85de65fade9fb39d13ec4
vagner0795 <vagner-se@hotmail.com>
2023-09-20 10:46:38 -0300

    feat: updated config, updated css and updated config script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,20 @@
-// const q = document.querySelectorAll('.q');
-// const a = document.querySelectorAll('.a');
-// const arr = document.querySelectorAll('.arrow');
-
-// // SELECIONAR TODOS OS ELEMENTOS 'Q'
-// for(let i = 0; i < q.length; i++) {
-//     q[i].addEventListener('click', () => {
-//         a[i].classList.toggle('a-oppened');
-//         arr[i].classList.toggle('arrow-rotated');
-
-//     });
-
-// }
-
-const q = document.querySelectorAll('.q');
-const a = document.querySelectorAll('.a');
-const arr = document.querySelectorAll('.arrow');
+const q = document.querySelectorAll(".q");
+const a = document.querySelectorAll(".a");
+const arr = document.querySelectorAll(".arrow");
 
 // SELECIONAR TODOS OS ELEMENTOS 'Q'
-for(let i = 0; i < q.length; i++) {
-  q[i].addEventListener('click', () => {
+for (let i = 0; i < q.length; i++) {
+  q[i].addEventListener("click", () => {
     // FECHAR TODAS AS RESPOSTAS ABERTAS
-    for(let j = 0; j < a.length; j++) {
-      if(j !== i && a[j].classList.contains('a-oppened')) {
-        a[j].classList.remove('a-oppened');
-        arr[j].classList.remove('arrow-rotated');
+    for (let j = 0; j < a.length; j++) {
+      if (j !== i && a[j].classList.contains("a-oppened")) {
+        a[j].classList.remove("a-oppened");
+        arr[j].classList.remove("arrow-rotated");
       }
     }
-    
+
     // ABRIR/FECHAR A RESPOSTA CLICADA
-    a[i].classList.toggle('a-oppened');
-    arr[i].classList.toggle('arrow-rotated');
+    a[i].classList.toggle("a-oppened");
+    arr[i].classList.toggle("arrow-rotated");
   });
 }
